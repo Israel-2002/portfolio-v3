022358fc06ea8f0a639fd12bc3af7ecc9043b2df
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-16 02:05:05 +0000

    updated navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import Button from "@/components/Button";
 import Container from "@/components/Container";
 import { cn } from "@/lib/cn";
 import { useState } from "react";
@@ -17,7 +16,7 @@ const Navbar = () => {
       <Container>
         <div className="grid-cols-3 items-center md:grid">
           <div className="flex w-full items-center justify-between">
-            <span className="cursor-pointer">[izzy]</span>
+            <span className="cursor-pointer text-primary">[izzy]</span>
             <span
               className="md:hidden"
               onClick={() => setIsOpen((prev) => !prev)}
@@ -31,11 +30,11 @@ const Navbar = () => {
               "absolute left-0 top-[85px] h-[390px] w-full bg-white/20 backdrop-blur-sm transition-transform duration-1000 md:static md:top-auto md:col-span-2 md:h-auto md:bg-transparent",
               isOpen
                 ? "-translate-y-0"
-                : "-translate-y-[120%] md:translate-y-0",
+                : "-translate-y-[125%] md:translate-y-0",
             )}
           >
             <div className="absolute left-0 top-[50px] grid w-full place-items-center md:static md:left-auto md:top-auto md:flex md:items-center md:justify-between">
-              <ul className="mb-12 flex flex-col gap-8 text-center md:mb-0 md:flex-row">
+              <ul className="mb-12 flex flex-col gap-12 text-center md:mb-0 md:flex-row">
                 <li>
                   <NavLink to="/projects">Projects</NavLink>
                 </li>
@@ -57,10 +56,10 @@ const Navbar = () => {
                   </a>
                 </div>
 
-                <Button
+                {/* <Button
                   label="Contact me"
                   className="hover:bg-primary transition-colors duration-300"
-                />
+                /> */}
               </div>
             </div>
           </div>
