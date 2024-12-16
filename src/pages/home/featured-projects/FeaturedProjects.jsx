f9c3bfaa42efe6bbf796e093b3f26e82e5921f27
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-16 02:07:14 +0000

    created the FeaturedProjects component

---
+++ src/pages/home/featured-projects/FeaturedProjects.jsx
@@ -0,0 +1,13 @@
+import Header from "@/pages/home/featured-projects/Header";
+import Projects from "@/pages/home/featured-projects/Projects";
+
+const FeaturedProjects = () => {
+  return (
+    <section>
+      <Header />
+      <Projects />
+    </section>
+  );
+};
+
+export default FeaturedProjects;
