8c850c42fbeb757726df81279a8c07d9dcaf168d
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-16 02:05:42 +0000

    created the projects constant

---
+++ src/constants/PROJECTS.js
@@ -0,0 +1,27 @@
+import img from "@/assets/images/projects/pottery-artist-portfolio.jpg";
+import img2 from "@/assets/images/projects/evergreen-solutions.jpg";
+import img3 from "@/assets/images/projects/echo-stream.jpg";
+import img4 from "@/assets/images/projects/pulse-tech.jpg";
+
+export const PROJECTS = [
+  {
+    img,
+    category: "Pottery Artist Portfolio",
+    title: "SilverLynx Technologies",
+  },
+  {
+    img: img2,
+    category: "Artist Portfolio",
+    title: "Evergreen Solutions",
+  },
+  {
+    img: img3,
+    category: "Landing Page",
+    title: "EchoStream Entertainment",
+  },
+  {
+    img: img4,
+    category: "Ecommerce Store",
+    title: "PulseTech Innovations",
+  },
+];
