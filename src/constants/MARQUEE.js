d53d29c01112a91f78a89280df7b0222890edcc4
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-16 12:52:48 +0000

    created marquee constant

---
+++ src/constants/MARQUEE.js
@@ -0,0 +1,18 @@
+export const PROJECTS_MARQUEE = [
+  {
+    number: "8+",
+    text: "years of experience",
+  },
+  {
+    number: ">95%",
+    text: "client retention rate",
+  },
+  {
+    number: "18",
+    text: "satisfied clients",
+  },
+  {
+    number: "14",
+    text: "projects finished",
+  },
+];
