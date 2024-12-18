df6acff6936df3a048491487f65e837db2b421d0
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-18 04:12:03 +0000

    added techstack

---
+++ src/constants/TECHSTACK.js
@@ -0,0 +1,51 @@
+import nextJs from "@/assets/svg/techstack/next-js.svg";
+import reactJs from "@/assets/svg/techstack/react-js.svg";
+import html from "@/assets/svg/techstack/html.svg";
+import css from "@/assets/svg/techstack/css.svg";
+import tailwind from "@/assets/svg/techstack/tailwind.svg";
+import sass from "@/assets/svg/techstack/sass.svg";
+import javascript from "@/assets/svg/techstack/javascript.svg";
+import typescript from "@/assets/svg/techstack/typescript.svg";
+
+export const TECHSTACK = [
+  {
+    icon: nextJs,
+    name: "Next JS",
+    description: "React framework for modern web apps.",
+  },
+  {
+    icon: reactJs,
+    name: "React JS",
+    description: "Library for building user interfaces.",
+  },
+  {
+    icon: html,
+    name: "HTML",
+    description: "Defines web page structure and content.",
+  },
+  {
+    icon: css,
+    name: "CSS",
+    description: "Styles and layouts for web pages.",
+  },
+  {
+    icon: tailwind,
+    name: "Tailwind CSS",
+    description: "Utility-first framework for custom designs.",
+  },
+  {
+    icon: sass,
+    name: "Sass",
+    description: "CSS preprocessor with extended features.",
+  },
+  {
+    icon: javascript,
+    name: "JavaScript",
+    description: "Adds interactivity and dynamic behavior.",
+  },
+  {
+    icon: typescript,
+    name: "TypeScript",
+    description: "JavaScript with static types added.",
+  },
+];
