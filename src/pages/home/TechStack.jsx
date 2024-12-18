4e5f898e2b497de521756d11e2fccec136a451f8
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-18 12:12:51 +0000

    made the title only stick on xl screens and above

--- src/pages/home/TechStack.jsx
+++ src/pages/home/TechStack.jsx
@@ -7,7 +7,7 @@ const TechStack = () => {
     <section className="bg-[#f8f8f8] py-[3.125rem] xl:py-[10rem]">
       <Container>
         <div className="mx-auto flex max-w-[1280px] flex-col items-start justify-between gap-[1.875rem] xl:flex-row">
-          <div className="sticky top-20 mx-auto w-fit text-center xl:mx-0 xl:text-left">
+          <div className="xl:sticky xl:top-20 mx-auto w-fit text-center xl:mx-0 xl:text-left">
             <div className="mx-auto w-fit xl:mx-0">
               <SubtitleText noShadow number="02" text="Tech Stack" />
             </div>
@@ -19,7 +19,7 @@ const TechStack = () => {
             {TECHSTACK.map(({ icon, name, description }, i) => (
               <li
                 key={i}
-                className="sticky top-60 flex w-full max-w-[520px] gap-[1.5rem] rounded-[14px] bg-white px-6 py-10 font-semibold shadow-md shadow-black/5 min-[400px]:p-10 xl:top-20 xl:mx-0 xl:max-w-[920px]"
+                className="sticky top-5 flex w-full max-w-[520px] gap-[1.5rem] rounded-[14px] bg-white px-6 py-10 font-semibold shadow-md shadow-black/5 min-[400px]:p-10 xl:top-20 xl:mx-0 xl:max-w-[920px]"
               >
                 <img
                   src={icon}
