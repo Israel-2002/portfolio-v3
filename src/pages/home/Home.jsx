250e93b5ddd653fa72775bcc0e7677915bf93b16
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-18 04:14:30 +0000

    rendered techstack

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 // import Hero from "@/pages/home/Hero";
 
 import FeaturedProjects from "@/pages/home/featured-projects/FeaturedProjects";
+import TechStack from "@/pages/home/TechStack";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       {/* <Hero /> */}
       <div className="h-screen bg-primary"></div>
       <FeaturedProjects />
+      <TechStack />
     </>
   );
 };
