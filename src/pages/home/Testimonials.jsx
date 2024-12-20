9879fcf8ec76fc8530f4fee1d97302d07581b54f
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-20 17:33:10 +0000

    added testimonials section

---
+++ src/pages/home/Testimonials.jsx
@@ -0,0 +1,67 @@
+import Container from "@/components/Container";
+import SubtitleText from "@/components/SubtitleText";
+import asterisk from "@/assets/svg/asterisk.svg";
+import quote from "@/assets/svg/quote.svg";
+import { TESTIMONIALS } from "@/constants/TESTIMONIALS";
+
+const Testimonials = () => {
+  const testimonials = Array(24).fill(TESTIMONIALS).flat();
+
+  return (
+    <section className="bg-black pb-[3.125rem] pt-6 text-white/90 xl:pb-[8.125rem] xl:pt-[1.875rem]">
+      <Container>
+        <div className="text-center">
+          <div className="mx-auto w-fit">
+            <SubtitleText number="03" text="Testimonials" />
+          </div>
+
+          <h2 className="">
+            Don&apos;t take my <br /> word for{" "}
+            <span className="relative">
+              it{" "}
+              <img
+                className="absolute -right-6 top-[20%] inline-block w-5 md:-right-8 md:w-7"
+                src={asterisk}
+                alt=""
+              />
+            </span>
+          </h2>
+          <p className="mb-10 mt-5 text-[0.9375rem] font-semibold text-grey xl:mb-[6.25rem] xl:mt-6 xl:text-[1.0625rem]">
+            <span className="text-primary">*</span>Take theirs
+          </p>
+        </div>
+      </Container>
+
+      <div className="overflow-hidden mask">
+        <ul className="marquee flex w-max gap-[1.875rem]">
+          {testimonials.map(({ name, image, testimonial }, i) => (
+            <li
+              key={i}
+              className="flex w-fit max-w-[320px] flex-shrink-0 flex-col rounded-[14px] border border-white/10 p-6 xl:max-w-[423px] xl:p-[3rem]"
+            >
+              <img
+                className="mb-10 ml-auto w-9 xl:w-[50px]"
+                src={quote}
+                alt=""
+              />
+              <p className="mb-[4.5rem] text-lg font-semibold leading-snug xl:text-[1.375rem]">
+                {testimonial}
+              </p>
+
+              <div className="mt-auto flex items-center gap-4 text-grey">
+                <img
+                  className="h-[46px] w-[46px] rounded-full object-cover"
+                  src={image}
+                  alt=""
+                />
+                <h4 className="mb-2 text-[0.9375rem] font-bold">{name}</h4>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
