0f3b4401e56f38c2c7495e44c67c98f2952d2477
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-20 22:25:40 +0000

    added socials section to homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@
 
 import Collaboration from "@/pages/home/Collaboration";
 import FeaturedProjects from "@/pages/home/featured-projects/FeaturedProjects";
+import Socials from "@/pages/home/Socials";
 import TechStack from "@/pages/home/TechStack";
 import Testimonials from "@/pages/home/Testimonials";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <TechStack />
       <Collaboration />
       <Testimonials />
+      <Socials />
     </>
   );
 };
