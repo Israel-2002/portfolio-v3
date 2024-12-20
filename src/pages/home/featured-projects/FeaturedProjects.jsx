b1b8bab6bed0de054b135ef5f5284d5db4354186
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-20 17:33:31 +0000

    rendered Marquee component

--- src/pages/home/featured-projects/FeaturedProjects.jsx
+++ src/pages/home/featured-projects/FeaturedProjects.jsx
@@ -1,11 +1,12 @@
+import Marquee from "@/components/Marquee";
+import { PROJECTS_MARQUEE } from "@/constants/MARQUEE";
 import Header from "@/pages/home/featured-projects/Header";
-import Marquee from "@/pages/home/featured-projects/Marquee";
 import Projects from "@/pages/home/featured-projects/Projects";
 
 const FeaturedProjects = () => {
   return (
     <section>
-      <Marquee />
+      <Marquee marqueeArr={PROJECTS_MARQUEE} />
       <Header />
       <Projects />
     </section>
