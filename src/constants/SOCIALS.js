ac09b2625847e30a2612e8db05fdac501c43005b
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-20 22:25:00 +0000

    created socials constant

---
+++ src/constants/SOCIALS.js
@@ -0,0 +1,36 @@
+import {
+  RiGithubLine,
+  RiLinkedinLine,
+  RiTelegram2Line,
+  RiTwitterXLine,
+} from "@remixicon/react";
+
+import curvedArrow from '@/assets/svg/curved-arrow.svg'
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/Israel-2002",
+    icon: RiGithubLine,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/israel-teye-dornor-5ba88725b",
+    icon: RiLinkedinLine,
+  },
+  {
+    name: "Twitter/X",
+    url: "https://x.com/Dreing16",
+    icon: RiTwitterXLine,
+  },
+  {
+    name: "Telegram",
+    url: "https://t.me/kiki1187",
+    icon: RiTelegram2Line,
+  },
+  {
+    name: "Get in touch",
+    url: "/contact",
+    icon: curvedArrow,
+  },
+];
