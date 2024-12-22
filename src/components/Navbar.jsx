860680d64240b434a458605679744bd17def21f4
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-22 17:04:28 +0000

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import Container from "@/components/Container";
 import { cn } from "@/lib/cn";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import arrowDiagonal from "@/assets/svg/arrow-45deg.svg";
+import arrowDiagonalBlack from "@/assets/svg/arrow-45deg-black.svg";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,59 +11,81 @@ const Navbar = () => {
   return (
     <nav
       className={cn(
-        "absolute left-0 top-0 z-50 w-full border-b py-[1.875rem] transition-all duration-1000 md:h-auto",
-        isOpen ? "h-[476px]" : "h-[85px]",
+        "absolute left-0 top-0 z-50 w-full py-[1.875rem] text-white transition-all duration-1000 md:h-auto",
       )}
     >
       <Container>
-        <div className="grid-cols-3 items-center md:grid">
-          <div className="flex w-full items-center justify-between">
-            <span className="cursor-pointer text-primary">[izzy]</span>
-            <span
-              className="md:hidden"
-              onClick={() => setIsOpen((prev) => !prev)}
+        <div className="items-center justify-between md:flex">
+          <div className="flex w-full items-center justify-between md:w-fit">
+            <span className="cursor-pointer text-xl">izzy®</span>
+
+            <div
+              className="grid gap-2 md:hidden"
+              onClick={() => setIsOpen(true)}
             >
-              Hamburger
-            </span>
+              <span className="h-[1px] w-8 bg-matte-white"></span>
+              <span className="h-[1px] w-8 bg-matte-white"></span>
+            </div>
           </div>
 
+          {isOpen && (
+            <div
+              onClick={() => setIsOpen(false)}
+              className="fixed left-0 top-0 h-full w-full bg-matte-black/20 backdrop-blur-sm md:hidden"
+            ></div>
+          )}
+
           <div
-            className={cn(
-              "absolute left-0 top-[85px] h-[390px] w-full bg-white/20 backdrop-blur-sm transition-transform duration-1000 md:static md:top-auto md:col-span-2 md:h-auto md:bg-transparent",
-              isOpen
-                ? "-translate-y-0"
-                : "-translate-y-[125%] md:translate-y-0",
-            )}
+            style={{
+              left: isOpen ? 0 : "-100%",
+            }}
+            className="transition-al absolute top-0 grid h-dvh w-[90%] max-w-[500px] place-items-center bg-white duration-300 md:static md:left-auto md:top-auto md:flex md:h-auto md:w-fit md:max-w-none md:items-center md:justify-between md:bg-transparent"
           >
-            <div className="absolute left-0 top-[50px] grid w-full place-items-center md:static md:left-auto md:top-auto md:flex md:items-center md:justify-between">
-              <ul className="mb-12 flex flex-col gap-12 text-center md:mb-0 md:flex-row">
-                <li>
-                  <NavLink to="/projects">Projects</NavLink>
-                </li>
-
-                <li>
-                  <NavLink>About</NavLink>
-                </li>
-
-                <li>
-                  <NavLink>Contact</NavLink>
-                </li>
-              </ul>
-
-              <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
-                <div className="md:hidden xl:block">
-                  <span className="opacity-60">Email:</span>
-                  <a href="mailto:israelrankin@gmail.com">
-                    israelrankin@gmail.com
-                  </a>
-                </div>
-
-                {/* <Button
-                  label="Contact me"
-                  className="hover:bg-primary transition-colors duration-300"
-                /> */}
-              </div>
+            <div
+              className="absolute right-6 top-[26px] grid py-4 md:hidden"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="h-[1px] w-7 rotate-45 bg-matte-black"></span>
+              <span className="h-[1px] w-7 -rotate-45 bg-matte-black"></span>
             </div>
+
+            <ul className="flex flex-col items-center gap-10 text-center text-lg text-matte-black md:flex-row md:gap-12 md:text-base md:text-white">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+
+              <li>
+                <NavLink>About</NavLink>
+              </li>
+
+              <li>
+                <NavLink>Contact</NavLink>
+              </li>
+
+              <li>
+                <a
+                  href="https://israeldornor.vercel.app/"
+                  target="_blank"
+                  className="flex items-center"
+                >
+                  Version 2
+                  <img
+                    className="hidden w-[30px] md:block"
+                    src={arrowDiagonal}
+                    alt=""
+                  />
+                  <img
+                    className="w-[30px] md:hidden"
+                    src={arrowDiagonalBlack}
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </Container>
