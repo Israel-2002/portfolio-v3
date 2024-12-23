5333c3cd9654cdc1aa325e342085f313263412ab
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-23 00:22:30 +0000

    modified hero

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -2,6 +2,7 @@ import Button from "@/components/Button";
 import Container from "@/components/Container";
 import arrowRight from "@/assets/svg/arrow-right.svg";
 import heroBg from "@/assets/images/home/hero-bg.png";
+import ReactCurvedText from "react-curved-text";
 
 const Hero = () => {
   return (
@@ -11,25 +12,42 @@ const Hero = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-      className="relative md:max-h-[700px] bg-matte-white pb-14 pt-48 text-matte-black lg:h-screen"
+      className="relative bg-matte-white pb-14 pt-48 text-matte-black md:max-h-[700px] lg:h-screen"
     >
       <Container>
-        <h1 className="font-medium text-white">
-          Delivering Thoughtfully <br /> Built Web{" "}
-          <span className="text-primary">Experiences</span>.
-        </h1>
+        <div className="relative">
+          <h1 className="font-medium text-white">
+            Delivering Thoughtfully <br /> Built Web{" "}
+            <span className="text-primary">Experiences</span>.
+          </h1>
 
-        <p className="mb-10 mt-5 max-w-[470px] text-lg min-[800px]:max-w-[700px] xl:text-xl">
-          I build fast, responsive websites that prioritize seamless user
-          experiences and modern design, ensuring every detail aligns with your
-          goals.
-        </p>
+          <p className="mb-10 mt-5 max-w-[470px] text-lg min-[800px]:max-w-[700px] xl:text-xl">
+            I build fast, responsive websites that prioritize seamless user
+            experiences and modern design, ensuring every detail aligns with
+            your goals.
+          </p>
 
-        <Button
-          icon={arrowRight}
-          label="See what I can do"
-          className="w-[250px] text-left"
-        />
+          <Button
+            icon={arrowRight}
+            label="See what I can do"
+            className="w-[250px] text-left"
+          />
+
+          <div className="absolute top-[30%] xl:top-[35%] right-[2%] hidden scale-[0.6] md:block lg:right-[8%] lg:scale-[0.7] xl:right-[15%]">
+            <ReactCurvedText
+              width={300}
+              height={300}
+              cx={150}
+              cy={150}
+              rx={60}
+              ry={60}
+              startOffset={0}
+              reversed={true}
+              svgProps={{ className: "rotating-curved-text" }}
+              text="FRONTEND DEVELOPER • FRONTEND DEVELOPER • "
+            />
+          </div>
+        </div>
       </Container>
     </header>
   );
