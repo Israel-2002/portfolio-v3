bf160ff7d02aa6e5ebb8dc1faec045ebac699456
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-24 22:09:12 +0000

    add project descriptions to enhance portfolio details

--- src/constants/PROJECTS.js
+++ src/constants/PROJECTS.js
@@ -8,20 +8,25 @@ export const PROJECTS = [
     img,
     category: "Pottery Artist Portfolio",
     title: "SilverLynx Technologies",
+    description: "Result: 175% increase in online sales.",
   },
   {
     img: img2,
     category: "Artist Portfolio",
     title: "Evergreen Solutions",
+    description: 'Featured in Best "Portfolio Websites of 2023" by DesignMag',
   },
   {
     img: img3,
     category: "Landing Page",
     title: "EchoStream Entertainment",
+    description: "Interface is streamlined for ease of navigation, with clean layouts"
   },
   {
     img: img4,
     category: "Ecommerce Store",
     title: "PulseTech Innovations",
+    description:
+      "Result: 200% increase in online sales within the first quarter",
   },
 ];
