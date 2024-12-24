a0027b1dc4f8be57ecff5c5193de678259ab4c80
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-24 22:07:19 +0000

    added projects route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from "@/pages/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/home/Home";
+import Projects from "@/pages/projects/Projects";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,6 +13,10 @@ function App() {
           index: true,
           element: <Home />,
         },
+        {
+          path: "projects",
+          element: <Projects />,
+        }
       ],
     },
   ]);
