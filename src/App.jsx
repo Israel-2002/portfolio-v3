2caa2a10a845737dbee702b771439661d1993746
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-26 20:13:57 +0000

    add Contact component and route to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "@/pages/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/home/Home";
 import Projects from "@/pages/projects/Projects";
+import Contact from "@/pages/contact/Contact";
 
 function App() {
   const router = createBrowserRouter([
@@ -16,7 +17,11 @@ function App() {
         {
           path: "projects",
           element: <Projects />,
-        }
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
       ],
     },
   ]);
