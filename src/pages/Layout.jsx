ceb4eeff1fe0d1b55605ffed2133026823839b12
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-26 20:14:44 +0000

    add scroll-to-top functionality on route change

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,15 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
