ec836d548315cb8e373b726985ada10baed2209c
Dornor Teye Israel <israelrankin@gmail.com>
2024-12-29 15:23:07 +0000

    add About page route to the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/home/Home";
 import Projects from "@/pages/projects/Projects";
 import Contact from "@/pages/contact/Contact";
+import About from "@/pages/about/About";
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,10 @@ function App() {
           path: "contact",
           element: <Contact />,
         },
+        {
+          path: "about",
+          element: <About />
+        }
       ],
     },
   ]);
