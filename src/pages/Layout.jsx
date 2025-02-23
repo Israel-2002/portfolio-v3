695ef57c2b2f40e6296682cdd72c1d797f646f69
izzy <israelrankin@gmail.com>
2025-02-23 09:48:04 +0000

    added lenis smooth scroll

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,21 +1,35 @@
+import gsap from "gsap";
+import { ReactLenis } from "lenis/react";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import "lenis/dist/lenis.css";
 
 const Layout = () => {
+  const lenisRef = useRef();
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 900);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </ReactLenis>
   );
 };
 
