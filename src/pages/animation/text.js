a08cc3b58f9ba6c762be601aa83c84b5ac07bc76
izzy <israelrankin@gmail.com>
2025-02-23 21:50:45 +0000

    add text animation functionality using GSAP and SplitType

---
+++ src/pages/animation/text.js
@@ -0,0 +1,58 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const text = (element) => {
+  let splitText;
+  let scrollTriggers = [];
+
+  const init = () => {
+    splitText = new SplitType(element, { types: "lines" });
+
+    splitText.lines?.forEach((line) => {
+      const mask = document.createElement("div");
+      mask.style.overflowY = "hidden";
+
+      if (line.parentElement.classList.contains("split-padding")) {
+        mask.style.paddingBottom = "7px";
+      }
+
+      mask.appendChild(line.cloneNode(true));
+      line.parentElement.replaceChild(mask, line);
+
+      const tween = gsap.from(mask.firstChild, {
+        yPercent: 100,
+        duration: 1,
+        scrollTrigger: {
+          trigger: mask,
+        },
+      });
+
+      ScrollTrigger.create({
+        trigger: mask,
+        start: "top 100%",
+        onLeaveBack: () => tween.pause(0),
+      });
+
+      scrollTriggers.push(tween.scrollTrigger);
+    });
+  };
+
+  const killScrollTriggers = () => {
+    scrollTriggers.forEach((scrollTrigger) => {
+      scrollTrigger.kill();
+    });
+  };
+
+  const handleResize = () => {
+    if (splitText) splitText.revert();
+    killScrollTriggers();
+    init();
+  };
+
+  init();
+
+  window.addEventListener("resize", handleResize);
+};
