a8d8748b605f482bd91972f231c7a0d81cc46310
izzy <israelrankin@gmail.com>
2025-10-23 00:10:22 +0000

    updated button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,37 @@
 import { cn } from "@/lib/cn";
 import PropTypes from "prop-types";
 
-const Button = ({ type = "button", label, onClick, className, icon }) => {
+const Button = ({
+  type = "button",
+  label,
+  onClick,
+  className,
+  icon,
+  disabled = false,
+  isLoading,
+}) => {
   return (
     <button
       type={type}
+      disabled={disabled}
       onClick={onClick}
       className={cn(
         "rounded-full bg-primary py-5 font-medium text-white md:text-lg",
         icon
-          ? "group relative pl-7 shadow-2xl shadow-primary/20 transition-shadow duration-300 hover:shadow-none"
+          ? "group relative flex items-center gap-2 pl-7 shadow-2xl shadow-primary/20 transition-shadow duration-300 hover:shadow-none"
           : "px-7",
         className,
       )}
     >
       {label}{" "}
-      {icon && (
+      {icon && typeof icon === "string" ? (
         <img
           className="absolute right-2 top-1/2 h-10 w-10 -translate-y-1/2 transition-all duration-300 group-hover:rotate-45"
           src={icon}
           alt=""
         />
+      ) : (
+        isLoading && icon
       )}
     </button>
   );
@@ -31,7 +42,9 @@ Button.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
   className: PropTypes.string,
-  icon: PropTypes.string,
+  icon: PropTypes.string | PropTypes.node,
+  isLoading: PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
 export default Button;
