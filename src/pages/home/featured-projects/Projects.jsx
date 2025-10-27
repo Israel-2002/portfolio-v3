bbc155c8f6d022eb29df4360050e95836d0c4804
izzy <israelrankin@gmail.com>
2025-10-27 16:22:39 +0000

    refactored component

--- src/pages/home/featured-projects/Projects.jsx
+++ src/pages/home/featured-projects/Projects.jsx
@@ -6,54 +6,92 @@ import arrowRight from "@/assets/svg/arrow-right.svg";
 import { Link } from "react-router-dom";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef } from "react";
-import { paralax } from "@/pages/animation/paralax";
+// import { useRef } from "react";
+// import { paralax } from "@/pages/animation/paralax";
+import PropTypes from "prop-types";
+import comingSoonImg from "@/assets/images/projects/coming-soon.jpg";
 
 gsap.registerPlugin(useGSAP);
 
+const Anchor = ({ url, category, title, img, comingSoon }) => {
+  return (
+    <a
+      href={url}
+      target="_blank"
+      className="group block px-6 pb-6 pt-[1.875rem] transition-colors lg:px-[1.375rem] lg:pb-[1.375rem] lg:pt-[4.375rem]"
+    >
+      <div className="split mb-[0.6875rem] flex items-center gap-0.5 text-sm font-semibold lg:mb-5 lg:text-[1.0625rem]">
+        <BracketOpen className="text-primary" />
+        {category}
+        <BracketClose className="text-primary" />
+      </div>
+
+      <h3 className="split mb-[4.0625rem] w-fit text-[1.625rem] md:mb-[2.125rem] lg:mb-[3.125rem] lg:text-[1.875rem]">
+        {title}
+      </h3>
+
+      <div
+        // ref={ref}
+        className="paralax-image-containe aspect-[16/10] overflow-hidden rounded-[10px] relative"
+      >
+        <div>
+          <img
+            className={cn(
+              !comingSoon
+                ? "transition-transform duration-300 group-hover:scale-110 object-cover"
+                : "w-full h-full absolute object-fit",
+            )}
+            src={comingSoon ? comingSoonImg : img}
+            alt={title}
+          />
+        </div>
+      </div>
+    </a>
+  );
+};
+
+Anchor.displayName = "Anchor";
+
+Anchor.propTypes = {
+  url: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  comingSoon: PropTypes.bool,
+};
+
 const Projects = () => {
-  const imageContainerRefs = useRef([]);
+  // const imageContainerRefs = useRef([]);
 
-  useGSAP(() => {
-    imageContainerRefs.current.forEach((container) => {
-      const img = container?.querySelector("img");
+  // useGSAP(() => {
+  //   imageContainerRefs.current.forEach((container) => {
+  //     const img = container?.querySelector("img");
 
-      paralax(img, container);
-    });
-  });
+  //     paralax(img, container);
+  //   });
+  // });
 
   return (
     <div id="work" className="bg-black text-white">
       <div className="lg:border-b lg:border-b-white/10">
         <ul className="mx-auto grid max-w-[1440px] lg:grid-cols-2">
-          {PROJECTS.slice(0, 2).map(({ category, img, title }, i) => (
+          {PROJECTS.slice(0, 2).map(({ category, img, title, url }, i) => (
             <li
               key={i}
               className={cn(
-                "border-b border-b-white/10 px-6 pb-6 pt-[1.875rem] lg:border-b-transparent lg:px-[1.375rem] lg:pb-[1.375rem] lg:pt-[4.375rem]",
+                "border-b border-b-white/10 lg:border-b-transparent",
                 i === 0
                   ? "lg:border-r lg:border-r-white/10 min-[1462px]:pl-0"
                   : "min-[1462px]:pr-0",
               )}
             >
-              <div className="split mb-[0.6875rem] flex items-center gap-0.5 text-sm font-semibold lg:mb-5 lg:text-[1.0625rem]">
-                <BracketOpen className="text-primary" />
-                {category}
-                <BracketClose className="text-primary" />
-              </div>
-
-              <h3 className="split mb-[4.0625rem] text-[1.625rem] md:mb-[2.125rem] lg:mb-[3.125rem] lg:text-[1.875rem]">
-                {title}
-              </h3>
-
-              <div
-                ref={(el) => imageContainerRefs.current.push(el)}
-                className="paralax-image-container aspect-[16/10] rounded-[10px]"
-              >
-                <div>
-                  <img className="object-cover" src={img} alt="" />
-                </div>
-              </div>
+              <Anchor
+                // ref={(el) => imageContainerRefs.current.push(el)}
+                url={url}
+                title={title}
+                category={category}
+                img={img}
+              />
             </li>
           ))}
         </ul>
@@ -61,36 +99,28 @@ const Projects = () => {
 
       <div className="mx-auto grid max-w-[1440px] lg:grid-cols-3">
         <ul className="grid lg:col-span-2 lg:grid-cols-2">
-          {PROJECTS.slice(2, 4).map(({ category, img, title }, i) => (
-            <li
-              key={i}
-              className={cn(
-                "flex flex-col border-b border-b-white/10 px-6 pb-6 pt-[1.875rem] lg:px-[1.375rem] lg:pb-[1.375rem] lg:pt-[4.375rem]",
-                i === 0
-                  ? "lg:border-r lg:border-r-white/10 min-[1462px]:pl-0"
-                  : "",
-              )}
-            >
-              <div className="split mb-[0.6875rem] flex items-center gap-0.5 text-sm font-semibold lg:mb-5 lg:text-[1.0625rem]">
-                <BracketOpen className="text-primary" />
-                {category}
-                <BracketClose className="text-primary" />
-              </div>
-
-              <h3 className="split mb-[4.0625rem] text-[1.625rem] md:mb-[2.125rem] lg:mb-[3.125rem] lg:text-[1.875rem]">
-                {title}
-              </h3>
-
-              <div
-                ref={(el) => imageContainerRefs.current.push(el)}
-                className="paralax-image-container mt-auto aspect-[16/10] rounded-[10px]"
+          {PROJECTS.slice(2, 4).map(
+            ({ url, category, img, title, comingSoon }, i) => (
+              <li
+                key={i}
+                className={cn(
+                  "flex flex-col border-b border-b-white/10",
+                  i === 0
+                    ? "lg:border-r lg:border-r-white/10 min-[1462px]:pl-0"
+                    : "",
+                )}
               >
-                <div>
-                  <img className="object-cover" src={img} alt="" />
-                </div>
-              </div>
-            </li>
-          ))}
+                <Anchor
+                  // ref={(el) => imageContainerRefs.current.push(el)}
+                  url={url}
+                  title={title}
+                  category={category}
+                  img={img}
+                  comingSoon={comingSoon}
+                />
+              </li>
+            ),
+          )}
         </ul>
 
         <div className="w-full px-6 lg:px-0">
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,23 +2,23 @@ import Container from "@/components/Container";
 import { PROJECTS } from "@/constants/PROJECTS";
 import BracketOpen from "@/assets/svg/bracket-open.svg?react";
 import BracketClose from "@/assets/svg/bracket-close.svg?react";
-import { useRef } from "react";
+// import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
-import { paralax } from "@/pages/animation/paralax";
+// import { paralax } from "@/pages/animation/paralax";
 import gsap from "gsap";
 
 gsap.registerPlugin(useGSAP);
 
 const Projects = () => {
-  const imageContainerRefs = useRef([]);
+  // const imageContainerRefs = useRef([]);
 
-  useGSAP(() => {
-    imageContainerRefs.current.forEach((container) => {
-      const img = container?.querySelector("img");
+  // useGSAP(() => {
+  //   imageContainerRefs.current.forEach((container) => {
+  //     const img = container?.querySelector("img");
 
-      paralax(img, container);
-    });
-  });
+  //     paralax(img, container);
+  //   });
+  // });
 
   return (
     <div className="pb-[3.25rem] pt-32 md:pt-[11.25rem] lg:pb-[5.625rem] xl:pb-[10rem] xl:pt-[14.375rem]">
@@ -33,36 +33,44 @@ const Projects = () => {
         </div>
 
         <ul className="mx-auto grid max-w-[1100px] gap-[4.375rem] text-[#2F2F2F] lg:gap-[5.625rem] xl:gap-[8.75rem]">
-          {PROJECTS.map(({ title, category, description, img }, i) => (
-            <li key={i}>
-              <div
-                ref={(el) => imageContainerRefs.current.push(el)}
-                className="paralax-image-container mb-8 h-[clamp(181px,calc(181px+(733-181)*((100vw-320px)/(1100-320))),733px)] overflow-hidden rounded-[14px] lg:mb-[3.125rem] lg:rounded-[20px] xl:mb-[4.375rem]"
-              >
-                <div>
-                  <img
-                    className="h-full w-full object-cover"
-                    src={img}
-                    alt=""
-                  />
-                </div>
-              </div>
+          {PROJECTS.map(
+            ({ title, category, description, img, url, comingSoon }, i) => {
+              if (comingSoon) return;
 
-              <span className="split mb-[1.125rem] flex items-center gap-0.5 text-sm font-semibold lg:text-[1.0625rem]">
-                <BracketOpen />
-                {category}
-                <BracketClose />
-              </span>
+              return (
+                <li key={i}>
+                  <a href={url} target="_blank" className="group">
+                    <div
+                      // ref={(el) => imageContainerRefs.current.push(el)}
+                      className="paralax-image-containe mb-8 h-[clamp(181px,calc(181px+(733-181)*((100vw-320px)/(1100-320))),733px)] overflow-hidden rounded-[14px] lg:mb-[3.125rem] lg:rounded-[20px] xl:mb-[4.375rem]"
+                    >
+                      <div>
+                        <img
+                          className="h-full w-full object-cover group-hover:scale-110 transition-transform duration-300"
+                          src={img}
+                          alt=""
+                        />
+                      </div>
+                    </div>
 
-              <h3 className="split mb-5 text-[2rem] leading-tight lg:mb-6 lg:text-[2.875rem] xl:mb-[1.75rem] xl:text-[3.75rem]">
-                {title}
-              </h3>
+                    <span className="split mb-[1.125rem] flex items-center gap-0.5 text-sm font-semibold lg:text-[1.0625rem]">
+                      <BracketOpen />
+                      {category}
+                      <BracketClose />
+                    </span>
 
-              <p className="split text-lg font-semibold leading-tight text-[#2F2F2F80] lg:text-xl xl:text-[1.375rem]">
-                {description}
-              </p>
-            </li>
-          ))}
+                    <h3 className="split mb-5 text-[2rem] leading-tight lg:mb-6 lg:text-[2.875rem] xl:mb-[1.75rem] xl:text-[3.75rem]">
+                      {title}
+                    </h3>
+
+                    <p className="split text-lg font-semibold leading-tight text-[#2F2F2F80] lg:text-xl xl:text-[1.375rem] max-w-[900px]">
+                      {description}
+                    </p>
+                  </a>
+                </li>
+              );
+            },
+          )}
         </ul>
       </Container>
     </div>
